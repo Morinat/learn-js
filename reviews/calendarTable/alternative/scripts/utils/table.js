9249fd59ad7fb89d15797f95d11bdedef99da426
gbezyuk <dev@gbezyuk.ru>
2022-04-09 13:20:52 +0300

    extract createPrimaryCalendarRow

--- reviews/calendarTable/alternative/scripts/utils/table.js
+++ reviews/calendarTable/alternative/scripts/utils/table.js
@@ -1,45 +1,56 @@
 import { getMonthNumberOfDays } from './date.js'
 
 const createDayNamesCalendarRow = () => {
-    let dayRow = document.createElement('tr')
-    let week = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс']
-    for (let j = 0; j < 7; j++) {
+    let dayRow = document.createElement('tr');
+    ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'].forEach(weekDayName => {
         let cell = document.createElement('th')
-        let cellText = document.createTextNode(week[j])
+        let cellText = document.createTextNode(weekDayName)
         cell.appendChild(cellText)
         dayRow.appendChild(cell)
-    }
+    })
     return dayRow
+}
+
+const createPrimaryCalendarRow = (dayCounter, rowIndex, firstWeekDay, numberOfDays) => {
+    let row = document.createElement('tr')
+    if (dayCounter > numberOfDays) {
+        return { row, dayCounter }
+    }
+    for (let j = 0; j < 7; j++) {
+        let cell = document.createElement('td')
+        let cellText
+        // inserting days
+        if (rowIndex === 0 && j === firstWeekDay) {
+            cellText = document.createTextNode(dayCounter)
+            dayCounter++
+        } else if (dayCounter <= numberOfDays && dayCounter > 1) {
+            cellText = document.createTextNode(dayCounter)
+            dayCounter++
+        } else {
+            cellText = document.createTextNode('')
+        }
+
+        cell.appendChild(cellText)
+        row.appendChild(cell)
+    }
+    return { row, dayCounter }
 }
 
 const createPrimaryCalendarRows = (firstWeekDay, numberOfDays) => {
     let dayCounter = 1
+    let row
     const rows = []
-    for (let i = 0; i < 5; i++) {
-        let row = document.createElement('tr')
-        for (let j = 0; j < 7; j++) {
-            let cell = document.createElement('td')
-            let cellText
-            // inserting days
-            if (i === 0 && j === firstWeekDay) {
-                cellText = document.createTextNode(dayCounter)
-                dayCounter++
-            } else if (dayCounter <= numberOfDays && dayCounter > 1) {
-                cellText = document.createTextNode(dayCounter)
-                dayCounter++
-            } else {
-                cellText = document.createTextNode('')
-            }
-
-            cell.appendChild(cellText)
-            row.appendChild(cell)
-        }
+    const numOfWeeks = 5
+    for (let i = 0; i < numOfWeeks; i++) {
+        ({ row, dayCounter } = createPrimaryCalendarRow(
+            dayCounter, i, firstWeekDay, numberOfDays
+        ))
         rows.push(row)
     }
     return rows
 }
 
-export function createCalendarTable (elem, year, month) {
+export const createCalendarTable = (elem, year, month) => {
     let firstDayOfMonth = new Date(year, month, 1)
     let firstWeekDay = firstDayOfMonth.getDay() - 1
     let numberOfDays = getMonthNumberOfDays(year, month)
